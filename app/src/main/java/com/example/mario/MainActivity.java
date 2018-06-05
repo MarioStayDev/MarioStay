5e1499601dd146291dad8695881802d844aa6b86
unknown <skhastagir98@gmail.comgit config user.name subho13>
2018-06-05 16:31:38 +0530

    Updates for the Navigation drawer

--- app/src/main/java/com/example/mario/MainActivity.java
+++ app/src/main/java/com/example/mario/MainActivity.java
@@ -5,13 +5,17 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -20,12 +24,13 @@ public class MainActivity extends AppCompatActivity
 	//private Toolbar mToolbar;
 	private final int REQUEST_LOGIN=101;
 	private final String KEY_SHARED_PREFERENCE="shared_pref_key",KEY_LOGGED_IN="logged_in_key";
+	private DrawerLayout mDrawerLayout;
 	private RecyclerView mList;
 	private DataAdapter mAdapter;
 	private SharedPreferences pfm;
 	private Toast mToast;
-	
-    @Override
+
+	@Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -34,13 +39,17 @@ public class MainActivity extends AppCompatActivity
 		Toolbar mToolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(mToolbar);
 
-		final DrawerLayout mDrawer = findViewById(R.id.drawer_layout);
+		ActionBar actionbar = getSupportActionBar();
+		actionbar.setDisplayHomeAsUpEnabled(true);
+		//actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+		mDrawerLayout = findViewById(R.id.drawer_layout);
 
 		NavigationView mNav = findViewById(R.id.navigation_view);
 		mNav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 			@Override
 			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-				mDrawer.closeDrawers();
+				mDrawerLayout.closeDrawers();
 				switch(item.getItemId()) {
 					case R.id.menu_add_prop:
 						d("Add prop");
@@ -52,6 +61,25 @@ public class MainActivity extends AppCompatActivity
 				return false;
 			}
 		});
+
+		ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar ,  R.string.app_name, R.string.app_name) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				//getActionBar().setTitle(mTitle);
+				//invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				//getActionBar().setTitle(mDrawerTitle);
+				//invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+
+		mDrawerToggle.syncState();
 		
 		mList = findViewById(R.id.list);
 		mList.setLayoutManager(new GridLayoutManager(this,2));
@@ -87,6 +115,10 @@ public class MainActivity extends AppCompatActivity
 				userIsLoggedIn = false;
 				Intent loginIntent = new Intent(this, LoginActivity.class);
 				startActivityForResult(loginIntent,REQUEST_LOGIN);
+				return true;
+			case android.R.id.home:
+				mDrawerLayout.openDrawer(GravityCompat.START);
+				return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
