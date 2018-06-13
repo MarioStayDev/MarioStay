c8f934177b5869d9b690ab24973226e8f46349e0
abhishek-360 <abhi.kumar310@gmail.com>
2018-06-13 13:15:52 +0530

    set Profile pic

--- app/src/main/java/com/example/mario/SignupActivity.java
+++ app/src/main/java/com/example/mario/SignupActivity.java
@@ -1,7 +1,15 @@
 package com.example.mario;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,7 +17,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignupActivity extends AppCompatActivity
 {
@@ -19,15 +30,61 @@ public class SignupActivity extends AppCompatActivity
 	private boolean male=true,stud=true;
 	private int OTG_LENGTH;
 
+	FloatingActionButton searchPic;
+	private CircleImageView profilePic;
+	private static int RESULT_LOAD_IMAGE=1;
+
+	private static final int REQUEST_WRITE_PERMISSION = 786;
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+	{
+		if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+		{
+			Intent i = new Intent(
+					Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+			startActivityForResult(i, RESULT_LOAD_IMAGE);
+		}
+	}
+
+	private void requestPermission()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+		{
+			requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+		} else
+		{
+			Intent i = new Intent(
+					Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+			startActivityForResult(i, RESULT_LOAD_IMAGE);
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.signup);
+
+		searchPic = (FloatingActionButton) findViewById(R.id.SearchPic);
+
+
+		searchPic.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				requestPermission();
+
+
+			}
+		});
 		
 		OTG_LENGTH=getResources().getInteger(R.integer.otp_length);
-		mToast=Toast.makeText(this,"",0);
+		mToast=Toast.makeText(this,"",Toast.LENGTH_SHORT);
 		mUser=findViewById(R.id.s_user);
 		mEmail=findViewById(R.id.s_email);
 		mPass=findViewById(R.id.s_pass);
@@ -131,5 +188,29 @@ public class SignupActivity extends AppCompatActivity
 		}
 
 	}
-	
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+
+			CircleImageView imageView = (CircleImageView) findViewById(R.id.ProPic);
+			imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+			// String picturePath contains the path of selected Image
+		}
+	}
 }
