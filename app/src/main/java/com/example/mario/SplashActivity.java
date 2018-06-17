d7124a66d6ccf6dc04b3593edea2acf0ca04972d
Subho <skhastagir98@gmail.com>
2018-06-17 17:42:25 +0530

    Bug fix : Splash Screen

--- app/src/main/java/com/example/mario/SplashActivity.java
+++ app/src/main/java/com/example/mario/SplashActivity.java
@@ -7,28 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity
 {
-	public static int SPLASH_TIME_OUT=2000;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-
-		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
-
-
-		new Handler().postDelayed(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				Intent loginIntent = new Intent(SplashActivity.this,LoginActivity.class);
-				startActivity(loginIntent);
-				finish();
-
-			}
-		},SPLASH_TIME_OUT);
+		startActivity(new Intent(this, MainActivity.class));
+		finish();
 	}
 	
 }
